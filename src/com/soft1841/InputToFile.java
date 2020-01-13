06eb7e2c4dc281be1fd0d9a096e244066c4c06a4
ll2313673906 <3340624204@qq.com>
2020-01-13 21:58:17 +0800

    util module created

---
+++ src/com/soft1841/InputToFile.java
@@ -0,0 +1,18 @@
+package com.soft1841;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class InputToFile{
+    public static void main(String[] args) throws IOException {
+        System.out.println("请输入：");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        //方法1：通过字符流输出
+        char[] chars = inputString.toCharArray();
+        File destFile1 = new File("E:/result1.txt");
+        Writer out1 =  new FileWriter(destFile1,true);
+        out1.write(chars);
+        out1.close();
+  }
+}
